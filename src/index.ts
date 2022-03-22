ad7bdffac2693a2b7ebc876d8e91cb09eb0b63ba
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 10:38:19 +0100

    feat: add TurboNumber

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 export const number = 5;
+
+export class TurboNumber {
+  constructor(public number: number) {}
+
+  subtract(x: number) {
+    this.number = this.number - x;
+    return this;
+  }
+
+  divide(x: number) {
+    // if (x === 0) throw new Error("Dont't divide by zero");
+    this.number = this.number / x;
+    return this;
+  }
+
+  result() {
+    return this.number;
+  }
+}
+
+export const turboNumber = new TurboNumber(10);
+
+console.log(turboNumber.divide(0));
+console.log(turboNumber.result());
+console.log(turboNumber.subtract(2));
+console.log(turboNumber.result());
+
+console.log();
+Number.MAX_SAFE_INTEGER;
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
