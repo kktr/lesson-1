79754332c7fd2129fe146d4881541866c3857bc2
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 13:42:02 +0100

    feat: refactor test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,21 @@
 import { TurboNumber } from './TurboNumber';
 
 describe('TurboNumber', () => {
+  let turboNumber: TurboNumber;
+
+  beforeEach(() => {
+    turboNumber = new TurboNumber(5);
+  });
+
   it('subtract 5 from 5', () => {
-    const turboNumber = new TurboNumber(5);
     expect(turboNumber.subtract(5).result()).toBe(0);
   });
 
   it('divide 5 by 5', () => {
-    const turboNumber = new TurboNumber(5);
     expect(turboNumber.divide(5).result()).toBe(1);
   });
 
   it('divide 5 by 0', () => {
-    const turboNumber = new TurboNumber(5);
     expect(() => turboNumber.divide(0)).toThrow('cannot divide by 0');
   });
 });
