a7aaa3c03186957ef72eca6242f44c1bf9eed03d
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 13:22:35 +0100

    test: setup test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,20 @@
 import { TurboNumber } from './TurboNumber';
 
 describe('TurboNumber', () => {
+  let turboNumber: TurboNumber;
+  beforeEach(() => {
+    turboNumber = new TurboNumber(10);
+  });
+
   it('subtract 5 form 10', () => {
-    const turboNumber = new TurboNumber(10);
     expect(turboNumber.subtract(5).result()).toBe(5);
   });
 
   it('divide 10 by 10', () => {
-    const turboNumber = new TurboNumber(10);
     expect(turboNumber.divide(10).result()).toBe(1);
   });
 
   it('divide 10 by 0', () => {
-    const turboNumber = new TurboNumber(10);
     expect(() => turboNumber.divide(0)).toThrow("can't divide by 0");
   });
 });
