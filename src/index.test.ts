770b08fce627db28f9d437ffc7fe146ce2a8e1d9
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 12:59:40 +0100

    test: refactor test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,30 +1,29 @@
 import { TurboNumber } from './index';
 
-describe('subtract', function () {
+describe('TurboNumber', function () {
+  let turboNumber: TurboNumber;
+
+  beforeEach(() => {
+    turboNumber = new TurboNumber(20);
+  });
+
   it('subtract 15 from 20', () => {
-    const turboNumber = new TurboNumber(20);
     expect(turboNumber.subtract(15).result()).toBe(5);
   });
 
   it('subtract 10 from 20', () => {
-    const turboNumber = new TurboNumber(20);
     expect(turboNumber.subtract(15).result()).toBe(5);
   });
-});
 
-describe('divide', function () {
   it('divide 20 by 20', () => {
-    const turboNumber = new TurboNumber(20);
     expect(turboNumber.divide(20).result()).toBe(1);
   });
 
   it('divide 20 by 10', () => {
-    const turboNumber = new TurboNumber(20);
     expect(turboNumber.divide(10).result()).toBe(2);
   });
 
   it('handle divide by 0', () => {
-    const turboNumber = new TurboNumber(20);
     expect(turboNumber.divide(0).result()).toBe(Infinity);
   });
 });
