c3944bb904a3f64fe9908b967267de2f4e02bd45
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 14:00:06 +0100

    feat: add dumb substract

---
+++ src/index.test.ts
@@ -0,0 +1,19 @@
+class TurboNumber {
+  constructor(public number: number) {}
+
+  subtract(x: number) {
+    this.number = 0;
+  }
+
+  result() {
+    return this.number;
+  }
+}
+
+describe('TurboNumber', () => {
+  it('subtract 2 from 2', () => {
+    const turboNumber = new TurboNumber(2);
+    turboNumber.subtract(2);
+    expect(turboNumber.result()).toBe(0);
+  });
+});
