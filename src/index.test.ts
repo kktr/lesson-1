40d91e10fa6ec6d5dc33ea893062bccf8ce59b4d
Krystian <sogestpodgorna4@gmail.com>
2022-03-22 12:42:48 +0100

    feat: add dumb substract implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,9 @@
-import { number, TurboNumber, turboNumber } from './index';
-
-let tn: TurboNumber;
-
-describe('number', function () {
-  it('works', () => {
-    expect(number).toBe(5);
-  });
-});
-
-describe('divide', function () {
-  beforeEach(() => {
-    tn = new TurboNumber(10);
-  });
-
-  it('divide 10 by 5', () => {
-    expect(tn.divide(2).result()).toBe(5);
-  });
-
-  it('divide 10 by 20', () => {
-    expect(tn.divide(20).result()).toBe(0.5);
-  });
-
-  it('divide 10 by 5', () => {
-    expect(TurboNumber.new(10).divide(5).result()).toBe(2);
-  });
-
-  it('divide 10 by -20', () => {
-    expect(tn.divide(-20).result()).toBe(-0.5);
-  });
-
-  it('divide 10 by 0', () => {
-    expect(tn.divide(0).result()).toBe(Infinity);
-  });
-});
+import { TurboNumber } from './index';
 
 describe('subtract', function () {
-  beforeEach(() => {
-    tn = new TurboNumber(10);
-  });
-
-  it('subtract 2 from 10', () => {
-    expect(tn.subtract(2).result()).toBe(8);
-  });
-
-  it('subtract 12 from 10', () => {
-    expect(tn.subtract(12).result()).toBe(-2);
-  });
-
-  it('subtract -0.5 from 10', () => {
-    expect(tn.subtract(-0.5).result()).toBe(10.5);
-  });
-
-  it('divide 10 by 0', () => {
-    expect(tn.divide(-20).result()).toBe(-0.5);
+  it('subtract 15 from 20 ', () => {
+    const turboNumber = new TurboNumber(20);
+    turboNumber.subtract(15);
+    expect(turboNumber.result()).toBe(5);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,9 @@ export const number = 5;
 export class TurboNumber {
   constructor(public number: number) {}
 
-  subtract(x: number) {
-    this.number = this.number - x;
-    return this;
-  }
-
-  divide(x: number) {
-    // if (x === 0) throw new Error("Dont't divide by zero");
-    this.number = this.number / x;
-    return this;
-  }
+  subtract(x: number) {}
 
   result() {
-    return this.number;
-  }
-
-  static new(x: number) {
-    return new TurboNumber(x);
+    return 5;
   }
 }
-
-export const turboNumber = new TurboNumber(10);
-
-console.log();
-console.log(turboNumber.result());
-console.log(turboNumber.subtract(2));
-console.log(turboNumber.result());
-
-console.log();
-Number.MAX_SAFE_INTEGER;
-
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MIN_SAFE_INTEGER);
